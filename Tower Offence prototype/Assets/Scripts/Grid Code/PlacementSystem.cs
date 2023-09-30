83dc9cdd0d8fc890da85260521445b4590d8f00a
Mixer-one <99894586+Mixer-one@users.noreply.github.com>
2023-09-30 21:44:15 +0100

    griddy 1.0

--- Tower Offence prototype/Assets/Scripts/Grid Code/PlacementSystem.cs
+++ Tower Offence prototype/Assets/Scripts/Grid Code/PlacementSystem.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class PlacementSystem : MonoBehaviour
 {
-    [SerializeField] private GameObject MouseIndicator;
+    [SerializeField] private GameObject MouseIndicator, CellIndicator;
     [SerializeField] private MouseTracker inputManager;
+    [SerializeField] private Grid grid;
 
     private void Update()
     {
         Vector3 MousePos = inputManager.GetMapPos();
+        Vector3Int GridPos = grid.WorldToCell(MousePos);
         MouseIndicator.transform.position = MousePos;
+        CellIndicator.transform.position = grid.CellToWorld(GridPos);
     }
 }
